99133d14fb3dcfeaeb45c31774fe3f193286bbbb
glori03 <glorigabriella@gmail.com>
2023-06-27 09:43:39 +0800

    adding mid project

---
+++ src/data/user.js
@@ -0,0 +1,79 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Tharindu N.",
+		email: "mail@tharindu.dev",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Gloria Gabriella Wowor.",
+		description:
+			"I am a student at Klabat University, faculty of computer science, majoring in information systems. I am a person who loves and is passionate about exploring the ever-evolving world of technology. As a technology professional, I have developed my knowledge and skills in various aspects such as software development, data analysis, artificial intelligence. I am always eager to learn new things and keep up with the latest developments.",
+	},
+
+	about: {
+		title: "I’m Gloria. I live in Indonesia, where I design the future.",
+		description:
+			"I have worked on various projects during my time in college and I am proud of the progress I have made.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Mobile Application Development.",
+
+			
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Data Visualization.",
+			
+			
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Advertising.",
+			
+			
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Business Intelligence and Analytics.",
+			
+			
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Research Methodology.",
+			
+			
+		},
+	],
+};
+
+export default INFO;
